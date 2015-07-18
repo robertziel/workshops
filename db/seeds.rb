d822c3fb2828f2f10bc3df59ab7353950643068d
robo-hobo <robohobo-95@o2.pl>
2015-07-18 13:42:24 +0200

    seeds.rb added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create({"firstname"=>"William", "lastname"=>"Shakespeare", "email"=>"williamshakespeare@gmail.com", "password"=>"qwertyuiop", "admin"=>true})
+User.create({"firstname"=>"Jonathan", "lastname"=>"Swift", "email"=>"jonathanswift@gmail.com", "password"=>"qwertyuiop"})
+User.create({"firstname"=>"Jane", "lastname"=>"Austen", "email"=>"janeausten@gmail.com", "password"=>"qwertyuiop"})
+User.create({"firstname"=>"行弘", "lastname"=>"松本", "email"=>"matsumotoyukihiro@gmail.com", "password"=>"qwertyuiop"})
+User.create({"firstname"=>"Joanne", "lastname"=>"Rowling", "email"=>"joannerowling@gmail.com", "password"=>"qwertyuiop"})
+User.create({"firstname"=>"Jan", "lastname"=>"Kowalski", "email"=>"jankowalski@gmail.com", "password"=>"qwertyuiop"})
